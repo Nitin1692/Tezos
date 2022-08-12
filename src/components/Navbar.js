f5bacb28934ee784dd48d6c0f4f297cd936cc56f
nitin <nitinjain7201@gmail.com>
2022-08-12 10:29:40 +0530

    Liquidity Baking

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+
+
+
+const Navbar = () => {
+ 
+  return (
+    <>
+    <div className="navbar navbar-dark bg-dark fixed-top">
+    <div className="container py-2">
+      <a href="/" className="navbar-brand">
+        Tezos Lottery
+      </a>
+    </div>
+  </div>
+  <div className="Box">
+  <button type="button" class="btn btn-primary"><a href="https://kolibri.finance/">Create Oven</a></button>
+  </div>
+  </>
+  );
+};
+
+export default Navbar;
